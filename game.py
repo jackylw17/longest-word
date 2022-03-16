e8e980bdde3536245efcf4cecf3e7cd0d9c5c213
jackylw17 <jackyrolland17@gmail.com>
2022-03-16 14:13:18 +0300

    Game development with TDD

---
+++ game.py
@@ -0,0 +1,27 @@
+# tests/test_game.py
+import unittest
+import string
+from game import Game
+
+class TestGame(unittest.TestCase):
+    def test_game_initialization(self):
+        new_game = Game()
+        grid = new_game.grid
+        self.assertIsInstance(grid, list)
+        self.assertEqual(len(grid), 9)
+        for letter in grid:
+            self.assertIn(letter, string.ascii_uppercase)
+# game.py
+
+# [...]
+
+    def is_valid(self, word):
+        if not word:
+            return False
+        letters = self.grid.copy() # Consume letters from the grid
+        for letter in word:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
+        return True
